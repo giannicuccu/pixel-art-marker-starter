a527eb0a86001a0b49b2da18098b34ba101acb8f
Gianni <gianni.cuccu@gmail.com>
2017-11-11 09:54:08 +0100

    first commit

--- designs.js
+++ designs.js
@@ -1,10 +1,20 @@
 // Select color input
 // Select size input
-
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+$("input[type=submit]").click(function(e){
+    e.preventDefault();
+    var color = $("#colorPicker").val();
+    var gridHeight = $("#input_height").val();
+    var gridWidth = $("#input_width").val();
+    makeGrid(color, gridHeight, gridWidth);
+})
+
+
+function makeGrid(color, gridHeight, gridWidth) {
 
 // Your code goes here!
-
+console.log(color);
+console.log(gridHeight);
+console.log(gridWidth);
 }
