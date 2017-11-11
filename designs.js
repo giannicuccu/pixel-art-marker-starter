d1805e2c7d3d0eb9f8206aab1fd245fad3b51f6a
Gianni <gianni.cuccu@gmail.com>
2017-11-11 19:17:46 +0100

    more opt, last commit with drag support

--- designs.js
+++ designs.js
@@ -1,12 +1,12 @@
 // Select color input
-var color = $("#colorPicker");
+const color = $('#colorPicker');
 
 // Select size input
-var gridWidth = $("#input_width");
-var gridHeight = $("#input_height");
+const gridWidth = $('#input_width');
+const gridHeight = $('#input_height');
 
 // When size is submitted by the user, call makeGrid()
-$("input[type=submit]").click(function (e) {
+$('input[type=submit]').click(function (e) {
     e.preventDefault();
     makeGrid(gridHeight.val(), gridWidth.val());
 })
@@ -14,23 +14,23 @@ $("input[type=submit]").click(function (e) {
 
 function makeGrid(gridHeight, gridWidth) {
     // Your code goes here!
-    var table = $("#pixel_canvas").empty();
-    var h = 0;
-    var mouseDownOnCanvas = false;
+    const table = $('#pixel_canvas').empty();
+    let h = 0;
+    let mouseDownOnCanvas = false;
     
-    // build the grid (element id's are not necessary)
+    // build the grid (added element id's are not necessary)
     while (h < gridHeight) {
         let element = h;
-        table.append("<tr id=tr_" + h + ">");
+        table.append('<tr id=tr_' + h + '>');
         for (let w = 0; w < gridWidth; w++) {
             const element = w;
-            $("<td id=td_" + h + "_" + w + ">").appendTo("#tr_" + h);
+            $('<td id=td_' + h + '_' + w + '>').appendTo('#tr_' + h);
         }
         h++ 
     }
 
-    // mouse events listeners to enable drag painting
-    // TODO what if mouse leave and reenter while clicked ???
+    // mouse events listeners to enable painting while user move the mouse with left button clicked 
+    // TODO: what if mouse leave the table and reenter while clicked ???
     table.mousedown(function () {
         mouseDownOnCanvas = true ;
     });
@@ -44,16 +44,16 @@ function makeGrid(gridHeight, gridWidth) {
     });
 
     //  mouse events listeners to paint the cell
-    $("#pixel_canvas td").each(function () {
+    $('#pixel_canvas td').each(function () {
         // paint <td> on click
         $(this).mousedown(function (e) {
             e.preventDefault();
-            $(this).css("background-color", $("#colorPicker").val());
+            $(this).css('background-color', $('#colorPicker').val());
         });
         //paint <td> on mouseenter if mouse button is clicked
         $(this).mouseenter(function () {
             if (mouseDownOnCanvas){
-                $(this).css("background-color", $("#colorPicker").val());
+                $(this).css('background-color', $('#colorPicker').val());
             }
         })
     })
