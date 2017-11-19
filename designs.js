8710ac67631f4d39534f13aa72f6378e93450aea
Gianni <gianni.cuccu@gmail.com>
2017-11-19 16:27:21 +0100

    FIX varours fix, added load create error messages

--- designs.js
+++ designs.js
@@ -7,17 +7,15 @@ $('input[type=submit]').click(function (e) {
 
 $('#saveDraw').click(function (e) {
     e.preventDefault();
-    draw.save();
-    message.updateMessage("Grid Saved");
-    //console.log(localStorage.getItem('untitledDraw'));
-
+    if (draw.save())
+        message.updateMessage("Grid Saved");
+    else
+        message.updateMessage("Nothing to save");
 });
 
 $('#loadDraw').click(function (e) {
     e.preventDefault();
     makeSavedGrid(draw.load());
-    message.updateMessage("Grid Loaded");
-
 });
 
 
@@ -27,8 +25,13 @@ function makeGrid() {
 };
 
 function makeSavedGrid(myGridObj) {
-    grid.insertGrid(grid.buildGridMarkupFromOjb(myGridObj));
-    grid.setHandlers();
+    try {
+        grid.insertGrid(grid.buildGridMarkupFromOjb(myGridObj));
+        grid.setHandlers();
+        message.updateMessage("Grid Loaded");
+    } catch (error) {
+        message.updateMessage("Nothing to load");
+    }
 };
 
 
@@ -47,7 +50,7 @@ const grid = {
             this.gridObjStruct.splice(0, this.gridObjStruct.length);
         }
     },
-    
+
     // generate table markup from form input and update gridObject
     buildGridMarkup: function (tableSize) {
         let gridMarkup = "";
@@ -55,7 +58,7 @@ const grid = {
         let cellId = 0;
         //save/load grid feature
         this.gridObj.init(tableSize.heigth, tableSize.width);
-        
+
 
         while (h < tableSize.heigth) {
             gridMarkup += '<tr id=tr_' + h + '>';
@@ -76,7 +79,7 @@ const grid = {
         let h = 0;
         let cellId = 0;
         // store width height 
-        this.gridObj.init(myGridObj.gridObjHeigth, myGridObj.gridObjWidth);        
+        this.gridObj.init(myGridObj.gridObjHeigth, myGridObj.gridObjWidth);
 
         while (h < myGridObj.gridObjHeigth) {
             gridMarkup += '<tr id=tr_' + h + '>';
@@ -88,7 +91,6 @@ const grid = {
             }
             gridMarkup += '</tr>'
             h++;
-
         }
         return gridMarkup;
     },
@@ -101,12 +103,18 @@ const grid = {
 
     // save the draw in local storage | TODO: named save feature
     saveGrid: function (gridObjName = 'untitledDraw') {
-        localStorage.setItem(gridObjName, JSON.stringify(grid.gridObj));
+        if (grid.gridObj.gridObjHeigth > 0 && grid.gridObj.gridObjWidth > 0) {
+            localStorage.setItem(gridObjName, JSON.stringify(grid.gridObj));
+            return true
+        } else {
+            return false;
+        }
     },
 
     // get saved draw object from local storage  | TODO: named save feature
     loadGrid: function (gridObjName) {
         let savedGrid = JSON.parse(localStorage.getItem(gridObjName));
+        //console.log(savedGrid);
         return savedGrid;
     },
 
@@ -148,9 +156,11 @@ const grid = {
 
 
 // Utilities 
+
+
 const colorPicker = {
     getColor: function () {
-        const color = $('#colorPicker').val();
+        let color = $('#colorPicker').val();
         return color;
     }
 }
@@ -158,9 +168,19 @@ const colorPicker = {
 
 const inputForm = {
     getTableSize: function () {
-        const tableSize = {
-            width: $('#input_width').val(),
-            heigth: $('#input_height').val()
+        let tableSize = {
+            width: parseInt($('#input_width').val()),
+            heigth: parseInt($('#input_height').val())
+        }
+        if (tableSize.width > 30) {
+            tableSize.width = 30;
+            console.log("max size error");
+            message.updateMessage("W set to max");
+        }
+        if (tableSize.heigth > 30) {
+            tableSize.heigth = 30;
+            message.updateMessage("H set to max");
+            console.log("max size error");
         }
         return tableSize;
     }
@@ -171,26 +191,25 @@ const message = {
     updateMessage: function (msg) {
         $('#message span').text("").fadeIn();
         $('#message span').text(msg).fadeOut('slow');
-
     }
 }
 
 
 const draw = {
     save: function (name) {
-        grid.saveGrid(name);
+        return grid.saveGrid(name);
     },
-
+    // TODO: 
     load: function (name = 'untitledDraw') {
         return grid.loadGrid(name);
     },
-    //TODO
+    // TODO:
     trash: function (draw) {
-         //TODO
+        //TODO
     },
-     //TODO
+    // TODO:
     list: function () {
-         //TODO
+        //TODO
     }
 
 }
